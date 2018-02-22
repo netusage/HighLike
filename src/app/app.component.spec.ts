a488aca7fb1454443760b37ff2e39ebd294a1558
opel <opel>
2018-02-22 16:02:07 +0200

    Render basic selection between 2 features

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { By } from '@angular/platform-browser';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -16,12 +21,22 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('High Like');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const element = fixture.debugElement.query(By.css('#title'));
+    expect(element.nativeElement.textContent).toBe('High Like');
+  }));
+  it('should select feature \'findCandidatesButton\' on init', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const element1 = fixture.debugElement.query(By.css('#findCandidatesButton'));
+      expect(element1.nativeElement.checked).toBe(true);
+      const element2 = fixture.debugElement.query(By.css('#generateProfileButton'));
+      expect(element2.nativeElement.checked).toBe(false);
+    });
   }));
 });
