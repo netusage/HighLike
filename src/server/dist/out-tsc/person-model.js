80c000b9aef7eead170e005cbd2cf47a3d671fd1
Iris <irisoron@HOTMAIL.COM>
2018-02-26 18:38:30 +0200

    merge

---
+++ src/server/dist/out-tsc/person-model.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PersonModel = /** @class */ (function () {
+    function PersonModel() {
+    }
+    return PersonModel;
+}());
+exports.PersonModel = PersonModel;
+//# sourceMappingURL=person-model.js.map
